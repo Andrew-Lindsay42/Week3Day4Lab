1c77dcfb24c5ec5f48e5eac8575b9da4f38b9dec
jhtdod <jess.dodge@hotmail.co.uk>
2021-12-02 13:53:35 +0000

    MVP finished

--- controllers/controller.py
+++ controllers/controller.py
@@ -6,3 +6,14 @@ from models.event import Event
 @app.route('/events')
 def index():
     return render_template('index.html', events = events)
+
+@app.route('/events', methods = ['POST'])
+def add_event():
+    date = request.form['date']
+    name = request.form['name']
+    guest_number = request.form['number']
+    room_location = request.form['location']
+    description = request.form['description']
+    new_event = Event(date, name, guest_number, room_location, description)
+    events.append(new_event)
+    return render_template('index.html', events = events)
