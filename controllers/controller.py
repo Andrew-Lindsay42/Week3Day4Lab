6a6213e0ed967ef58442f1a8c752ed911264685b
jhtdod <jess.dodge@hotmail.co.uk>
2021-12-02 15:48:21 +0000

    added an edit option

--- controllers/controller.py
+++ controllers/controller.py
@@ -10,7 +10,6 @@ def index():
 
 @app.route('/events', methods = ['POST'])
 def add_event():
-    print(request.form)
     date = request.form['date']
     name = request.form['name']
     guest_number = request.form['number']
@@ -27,4 +26,24 @@ def add_event():
 @app.route('/events/delete/<int:index>', methods = ['POST'])
 def delete_event(index):
     events.pop(index)
+    return redirect ('/events')
+
+@app.route('/events/edit/<int:index>', methods = ['POST'])
+def edit_event(index):
+    return render_template('edit.html', index=index, event = events[index])
+
+@app.route('/events/edit/submit/<int:index>', methods = ['POST'])
+def edit_event_submit(index):
+    date = request.form['date']
+    name = request.form['name']
+    guest_number = request.form['number']
+    room_location = request.form['location']
+    description = request.form['description']
+    if "recurring" in request.form:
+        recurring = True
+    else:
+        recurring = False
+    new_event = Event(date, name, guest_number, room_location, description, recurring)
+    events.insert(index, new_event)
+    events.pop(index + 1)
     return redirect ('/events')
